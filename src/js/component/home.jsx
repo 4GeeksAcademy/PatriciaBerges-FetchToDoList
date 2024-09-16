c80487740eaccf2b37179a594c45a56988fe1348
Patricia-Berges <patry37.zgz@gmail.com>
2024-09-16 19:34:54 +0000

    Almost all functions working properly

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
+
+	const [taskList, setTaskList] = useState(["Wash the dishes", "Clean the house"])
+	const [inputValue, setInputValue] = useState("")
+
+
+	const inputChange = (e) => {
+		setInputValue(e.target.value)
+	}
+
+	const handleKeyPress = (e) => {
+		if(e.key === "Enter"){
+			inputValue.length > 0 && setTaskList([...taskList, inputValue])
+			setInputValue("")
+		}
+	}
+
+	const deleteTask = (e) => {
+		let taskToDelete = taskList.filter((task, index) => {if(e.target.className == task) {
+			let newList = taskList
+			newList.splice(index,1)
+			setTaskList(newList)
+		}})
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<input type="text" value={inputValue} onChange={inputChange} onKeyDown={handleKeyPress} placeholder="Enter your task"/>
+			<ul>
+				{(taskList.length > 0) && taskList.map((task, index) => 
+				(
+					<div className="d-flex">
+						<li key={index}>{task}</li>
+						<button className={task} onClick={deleteTask}>X</button>
+					</div>
+				)
+				)}
+			</ul>
 		</div>
 	);
 };
