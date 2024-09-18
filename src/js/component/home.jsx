5d45bc73208aac887388b522193758e2812a3569
Patricia-Berges <patry37.zgz@gmail.com>
2024-09-18 11:33:45 +0000

    Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -20,26 +20,33 @@ const Home = () => {
 	}
 
 	const deleteTask = (e) => {
-		let taskToDelete = taskList.filter((task, index) => {if(e.target.className == task) {
-			let newList = taskList
-			newList.splice(index,1)
-			setTaskList(newList)
-		}})
+		const newList = taskList.filter((task) => task !== e.target.className)
+		setTaskList(newList)
+	}
+
+	const tasksLeft = () => {
+		if(taskList.length > 1) return (<p className=" tasksLeft">{taskList.length} tasks left to do</p>)
+		else if (taskList.length == 1) return (<p className=" tasksLeft">1 task left to do</p>)
+		else return (<p className=" tasksLeft">No tasks to do, add new tasks</p>)
 	}
 
 	return (
 		<div>
-			<input type="text" value={inputValue} onChange={inputChange} onKeyDown={handleKeyPress} placeholder="Enter your task"/>
-			<ul>
-				{(taskList.length > 0) && taskList.map((task, index) => 
-				(
-					<div className="d-flex">
-						<li key={index}>{task}</li>
-						<button className={task} onClick={deleteTask}>X</button>
-					</div>
-				)
-				)}
-			</ul>
+			<h1 className="fst-italic">todos</h1>
+			<div className="toDoList d-flex flex-column justify-content-center p-0">
+				<ul className="list-group">
+					<input className="list-group-item task" type="text" value={inputValue} onChange={inputChange} onKeyDown={handleKeyPress} placeholder="What needs to be done?"/>
+					{(taskList.length > 0) && taskList.map((task, index) => 
+					(
+						<div className="d-flex parent">
+							<li className="list-group-item task" key={index}>{task}</li>
+							<button className={task} onClick={deleteTask}>x</button>
+						</div>
+					)
+					)}
+					{tasksLeft()}
+				</ul>
+			</div>
 		</div>
 	);
 };
